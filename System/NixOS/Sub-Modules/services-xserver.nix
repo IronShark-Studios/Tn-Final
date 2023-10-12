eb819ea9c4d228ba7b94ca2056f1386926c9ecc6
Que <git@ironshark.org>
2023-10-12 10:45:49 -0400

    integrated xfce configuration

--- System/NixOS/Sub-Modules/services-xserver.nix
+++ System/NixOS/Sub-Modules/services-xserver.nix
@@ -1,6 +1,80 @@
 { inputs, outputs, lib, config, pkgs, ... }: {
 
+  environment = {
+    systemPackages = with pkgs; [
+      blueman
+      chromium
+      deja-dup
+      drawing
+      elementary-xfce-icon-theme
+      evince
+      firefox
+      foliate
+      font-manager
+      gimp-with-plugins
+      gnome.file-roller
+      gnome.gnome-disk-utility
+      inkscape-with-extensions
+      libqalculate
+      libreoffice
+      orca
+      pavucontrol
+      qalculate-gtk
+      thunderbird
+      wmctrl
+      xclip
+      xcolor
+      xcolor
+      xdo
+      xdotool
+      xfce.catfish
+      xfce.gigolo
+      xfce.orage
+      xfce.xfburn
+      xfce.xfce4-appfinder
+      xfce.xfce4-clipman-plugin
+      xfce.xfce4-cpugraph-plugin
+      xfce.xfce4-dict
+      xfce.xfce4-fsguard-plugin
+      xfce.xfce4-genmon-plugin
+      xfce.xfce4-netload-plugin
+      xfce.xfce4-panel
+      xfce.xfce4-pulseaudio-plugin
+      xfce.xfce4-systemload-plugin
+      xfce.xfce4-weather-plugin
+      xfce.xfce4-whiskermenu-plugin
+      xfce.xfce4-xkb-plugin
+      xfce.xfdashboard
+      xorg.xev
+      xsel
+      xtitle
+      xwinmosaic
+      zuki-themes
+    ];
+  };
+
+  programs = {
+    dconf.enable = true;
+    gnupg.agent = {
+      enable = true;
+      enableSSHSupport = true;
+    };
+
+    thunar = {
+      enable = true;
+      plugins = with pkgs.xfce; [
+        thunar-archive-plugin
+        thunar-media-tags-plugin
+        thunar-volman
+      ];
+    };
+  };
+
+  security.pam.services.gdm.enableGnomeKeyring = true;
+
   services = {
+    blueman.enable = true;
+    gnome.gnome-keyring.enable = true;
     pipewire = {
       enable = true;
       pulse.enable = true;
@@ -17,10 +91,25 @@
       layout = "us";
       xkbVariant = "colemak_dh";
       xkbOptions = "caps:escape";
+      excludePackages = with pkgs; [
+        xterm
+      ];
 
       displayManager = {
-        sddm.enable = true;
-        sddm.autoNumlock = true;
+        lightdm = {
+          enable = true;
+          greeters.slick = {
+            enable = true;
+            theme.name = "Zukitre-dark";
+          };
+        };
+      };
+
+      desktopManager.xfce = {
+        enable = true;
+     #   noDesktop = true;
+     #   enableXfwm = false;
+     #   enableScreensaver = false;
       };
     };
 
@@ -28,6 +117,13 @@
     picom.enable = true;
   };
 
-  sound.enable = true;
-  hardware.pulseaudio.enable = false;
+  sound = {
+    enable = true;
+    mediaKeys.enable = true;
+  };
+
+  hardware = {
+    pulseaudio.enable = false;
+    bluetooth.enable = true;
+  };
 }
