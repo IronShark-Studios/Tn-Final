0acbb75870e67e62bc4421c867e41f26c6fbfbc4
xin <git@ironshark.org>
2023-10-25 17:23:45 -0400

    thanatos Rebuilt:   69   2023-10-23 18:45:25

--- Thanatos/Home-Manager/Home-Packages/alacritty.nix
+++ Thanatos/Home-Manager/Home-Packages/alacritty.nix
@@ -16,44 +16,36 @@
 
       save_to_clipboard: true
 
-      padding = { x = 1 , y = 1 }
+      padding:
+        x: 1
+        y: 1
 
-      font = {
-        family = "Iosevka"
-        size = 18
-        weight = "Regular"
-        italic = true
-        bold = true
-        ligatures = true
-        monospace = true
-      }
+      font:
+        family: "Iosevka"
+        size: 16
+        weight: "Regular"
+        ligatures: true
+        monospace: "Iosevka Mono"
 
-      colors = {
-        primary = {
-           background = "#191e24"
-           foreground = "C5C8C6"
-        }
-        cursor = {
-          text = "#f8f8f2"
-          background = "#5ec4ff"
-        }
-      }
+      colors:
+        primary:
+           background: "#191e24"
+           foreground: "#C5C8C6"
+        cursor:
+          text: "#f8f8f2"
+          background: "#5ec4ff"
 
-      bell = "visual"
+      visual_bell:
+        animation: EaseOutExpo
+        duration: 0
+        color: '0xffffff'
 
-      cursor = "block"
-      cursor_blink = true
+      cursor:
+        style: "block"
+        blinking: Always
+        blink_interval: 500
 
-      key_bindings = {
-        "ctrl+q" = "send_signal SIGINT"
-        "ctrl+shift+space" = "set_mode Vi"
-        "escape" = "send_command /path/to/set-vi-mode-to-escape.sh"
-        "u" = "enter_insert_mode"
-        "n" = "move_down"
-        "e" = "move_up"
-        "m" = "move_left"
-        "i" = "move_right"
-      }
+
     '';
   };
 
