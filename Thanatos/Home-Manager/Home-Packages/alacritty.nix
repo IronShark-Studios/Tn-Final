fe6c905cdf034ef3239e01460ce726450f69b643
xin <git@ironshark.org>
2023-10-26 19:33:13 -0400

    thanatos Rebuilt:   76   2023-10-26 18:05:23

--- Thanatos/Home-Manager/Home-Packages/alacritty.nix
+++ Thanatos/Home-Manager/Home-Packages/alacritty.nix
@@ -44,11 +44,11 @@
 
       key_bindings:
         - { key: Space, mods: Shift, action: "ToggleViMode" }
-        - { key: u, mode: "Vi" action: "ToggleViMode" }
-        - { key: m, mode: "Vi" action: "Left" }
-        - { key: n, mode: "Vi" action: "Down" }
-        - { key: e, mode: "Vi" action: "Up" }
-        - { key: i, mode: "Vi" action: "Right" }
+        - { key: u, mode: "Vi", action: "ToggleViMode" }
+        - { key: m, mode: "Vi", action: "Left" }
+        - { key: n, mode: "Vi", action: "Down" }
+        - { key: e, mode: "Vi", action: "Up" }
+        - { key: i, mode: "Vi", action: "Right" }
 
     '';
   };
