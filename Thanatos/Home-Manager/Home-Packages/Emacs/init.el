9b44bd023b470197177bd3188fa8513da44c4da3
xin <git@ironshark.org>
2023-10-30 20:12:30 -0400

    thanatos Rebuilt:  100   2023-10-30 19:55:40

--- Thanatos/Home-Manager/Home-Packages/Emacs/init.el
+++ Thanatos/Home-Manager/Home-Packages/Emacs/init.el
@@ -341,7 +341,7 @@
                "\\*.+\\*"
                   )))
 
-;(global-set-key (kbd "M-x") 'helm-M-x)
+(global-set-key (kbd "M-x") 'helm-M-x)
 
 (global-set-key (kbd "C-x C-f") 'helm-find-files)
 
