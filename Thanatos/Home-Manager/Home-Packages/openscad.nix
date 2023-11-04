5d8e9700106a5cb66e0e5ed3f516c6697c543da2
xin <git@ironshark.org>
2023-11-04 12:06:53 -0400

    Rebuild:  257   2023-11-04 11:49:48    added openscad conf file

--- Thanatos/Home-Manager/Home-Packages/openscad.nix
+++ Thanatos/Home-Manager/Home-Packages/openscad.nix
@@ -5,10 +5,29 @@
       openscad
     ];
 
-    # file."tridactyl.nix" = {
-    #   target = ".config/tridactyl/tridactylrc";
-    #   text = ''
-    #   '';
-    #   };
+    file."OpenSCAD.conf" = {
+      target = ".config/OpenSCAD/OpenSCAD.conf";
+      text = ''
+           [General]
+           recentFileList=@Invalid()
+
+           [3dview]
+           colorscheme=DeepOcean
+
+           [design]
+           autoReload=true
+
+           [view]
+           hide3DViewToolbar=true
+           hideConsole=true
+           hideCustomizer=true
+           hideEditor=true
+           hideEditorToolbar=true
+           hideErrorLog=true
+           orthogonalProjection=true
+           showAxes=true
+           showScaleProportional=true
+        '';
+      };
     };
 }
