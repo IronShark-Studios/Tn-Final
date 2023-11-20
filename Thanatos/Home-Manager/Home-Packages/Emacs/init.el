658f0f492cc6d958de58441b6db02ca0e39d7afe
xin <git@ironshark.org>
2023-11-20 15:26:17 -0500

    Rebuild:  300   2023-11-19 19:42:17    added things to org agenda

--- Thanatos/Home-Manager/Home-Packages/Emacs/init.el
+++ Thanatos/Home-Manager/Home-Packages/Emacs/init.el
@@ -553,16 +553,11 @@
   (auto-fill-mode 0)
   (visual-line-mode 1))
 
-(setq evil-auto-indent nil
-      org-src-preserve-indentation nil
-      org-edit-src-content-indentation 0
-      org-fontify-done-headline t
-      org-enforce-todo-dependencies t
-      org-export-with-todo-keywords nil)
+(add-hook 'org-capture-mode-hook 'evil-insert-state)
+(add-hook 'org-log-buffer-setup-hook 'evil-insert-state)
 
 (defun Tn/org-font-setup ()
-
-;; This is magic conde that changes the font of non-heading bullet point lists.
+;; This is magic code that changes the font of non-heading bullet point lists.
 (font-lock-add-keywords 'org-mode
                         '(("^ *\\([-]\\) "
                            (0 (prog1 () (compose-region (match-beginning 1) (match-end 1) "•"))))))
@@ -575,18 +570,20 @@
                 (org-level-6 . "Azure3")
                 (org-level-7 . "Azure3")
                 (org-level-8 . "Azure3")))
-  (set-face-attribute (car face) nil :font "FiraGO"
-                      :weight 'regular :height 1.2
+  (set-face-attribute (car face) nil :font "Iosevka"
+                      :weight 'regular :height 1.3
                       :foreground (cdr face)))
 
+(set-face-attribute 'org-link nil    :foreground "dark cyan" :inherit 'fixed-pitch)
+(set-face-attribute 'org-tag nil    :foreground nil :height 0.9 :inherit 'fixed-pitch)
 (set-face-attribute 'org-block nil    :foreground nil :inherit 'fixed-pitch)
 (set-face-attribute 'org-table nil    :inherit 'fixed-pitch)
 (set-face-attribute 'org-formula nil  :inherit 'fixed-pitch)
 (set-face-attribute 'org-table nil    :inherit '(shadow fixed-pitch))
 (set-face-attribute 'org-code nil     :foreground "SpringGreen3"
-                    :inherit '(shadow fixed-pitch))
+                   :weight 'semi-bold :inherit '(shadow fixed-pitch))
 (set-face-attribute 'org-verbatim nil :foreground "SpringGreen3"
-                    :inherit '(shadow fixed-pitch))
+                   :weight 'semi-bold :inherit '(shadow fixed-pitch))
 (set-face-attribute 'org-special-keyword nil :inherit '(font-lock-comment-face fixed-pitch))
 (set-face-attribute 'org-meta-line nil :inherit '(font-lock-comment-face fixed-pitch))
 (set-face-attribute 'org-checkbox nil  :inherit 'fixed-pitch)
@@ -640,6 +637,9 @@ it can be passed in POS."
   (when (derived-mode-p 'org-mode)
     (Tn/org-set-time-file-property "LAST_MODIFIED")))
 
+(defun Tn/current-year () (interactive)
+  (shell-command-to-string "echo -n $(date +%Y)"))
+
 (setq org-capture-templates
   '(("j" "Journal Entry"
          (org-journal-open-current-journal-file)
@@ -651,11 +651,9 @@ it can be passed in POS."
          "* %<%H:%M> \n ** TODO %?"
          :empty-lines 1)))
 
-(add-hook 'org-capture-mode-hook 'evil-insert-state)
-
 (setq org-todo-keywords
-      (quote ((sequence "TODO(t)" "NEXT(n)" "ACTIVE(a)" "|" "DONE(d@/!)")
-              (sequence "WAITING(w@/!)" "HOLD(h@/!)" "|" "CANCELLED(c@/!)"))))
+      (quote ((sequence "TODO(t)" "NEXT(n)" "ACTIVE(a@/!)" "|" "DONE(d@/!)")
+              (sequence "WAITING(w@/!)" "INACTIVE(i@/!)" "HOLD(h@/!)" "|" "CANCELLED(c@/!)"))))
 
 (setq org-todo-keyword-faces
       (quote (("TODO" :foreground "deep sky blue" :weight bold)
@@ -665,6 +663,22 @@ it can be passed in POS."
               ("WAITING" :foreground "blue violet" :weight bold)
               ("HOLD" :foreground "dark red" :weight bold)
               ("CANCELLED" :foreground "dim gray" :weight bold))))
+
+(setq org-tag-alist
+      '((:startgroup . ART)
+        ("SCULPTURE" . ?s) ("ILLUSTRATION" . ?i) ("METAL-WORKING" . ?m)
+        (:endgroup . ART)
+        (:startgroup . TECHNOLOGY)
+        ("PROGRAMMING" . ?p) ("CAD" . ?x)
+        (:endgroup . TECHNOLOGY)
+        (:startgroup . ACADEMIC)
+        ("MATHS" . ?m)
+        (:endgroup . ACADEMIC)
+        (:startgroup . FITNESS)
+        ("@CF-GYM" . ?g) ("@OLY-GYM" . ?o)
+        (:endgroup . FITNESS)
+        ("@Toby" . ?T)
+        ("@PHONE" . ?P) ("@COMPUTER" . ?C) ("@CAR" . ?V)))
 
 (setq org-ellipsis " ▾"
       org-hide-emphasis-markers t
@@ -678,4 +692,164 @@ it can be passed in POS."
       org-startup-with-inline-images t
       org-cycle-separator-lines 2
       org-confirm-babel-evaluate nil
-      org-capture-bookmark nil)
+      org-catch-invisible-edits "error"
+      org-capture-bookmark nil
+      org-return-follows-link t
+      org-blank-before-new-entry t
+      evil-auto-indent nil
+      org-src-preserve-indentation nil
+      org-edit-src-content-indentation 0
+      org-fontify-done-headline t
+      org-enforce-todo-dependencies t
+      org-enforce-todo-checkbox-dependencies t
+      org-export-with-todo-keywords nil
+      org-reverse-note-order t
+      org-odd-levels-only t
+      org-directory "~/Archive/Feronomicon/ORG/"
+      org-archive-location (format
+                            "~/Archive/Feronomicon/ORG/\%s-archive.org::datetree/"
+                            (Tn/current-year)))
+
+(use-package org
+:hook
+(org-mode . Tn/org-mode-setup)
+(org-mode . Tn/org-font-setup)
+(before-save . Tn/org-set-last-modified)
+
+:config
+(org-babel-do-load-languages
+ 'org-babel-load-languages
+ '((emacs-lisp . t)
+   (lisp . t)
+   (latex . t)
+   (scheme . t)))
+
+(push '("conf-unix" . conf-unix) org-src-lang-modes))
+
+(global-set-key (kbd "C-c C-l") 'org-store-link)
+(global-set-key (kbd "C-c l") 'org-insert-link)
+
+(use-package org-roam
+  :bind (("C-c n l" . org-roam-buffer-toggle)
+         ("C-c n f" . org-roam-node-find)
+         ("C-c n g" . org-roam-graph)
+         ("C-c n i" . org-roam-node-insert)
+         ("C-c n c" . org-roam-capture))
+
+  :config
+  (setq org-roam-directory (file-truename "~/Archive/Grimoire/")
+        org-roam-node-display-template (concat "${title:*} " (propertize "${tags:10}" 'face 'org-tag)))
+
+  (org-roam-db-autosync-mode)
+  (require 'org-roam-protocol))
+
+(add-to-list 'display-buffer-alist
+             '("\\*org-roam\\*"
+               (display-buffer-in-direction)
+               (direction . right)
+               (window-width . 0.33)
+               (window-height . fit-window-to-buffer)))
+
+;; (("d" "default" plain "%?"
+;;   :target (file+head "%<%Y%m%d%H%M%S>-${slug}.org"
+;;                      "#+title: ${title}\n")
+;;   :unnarrowed t))
+
+;; (setq org-roam-dailies-capture-templates
+;;       '(("d" "default" entry
+;;          "* %?"
+;;          :target (file+head "%<%Y-%m-%d>.org"
+;;                             "#+title: %<%Y-%m-%d>\n"))))
+
+(use-package org-roam-bibtex
+  :after org-roam
+  :config
+  (require 'org-ref))
+
+(use-package org-roam-ui
+    :config
+    (setq org-roam-ui-sync-theme t
+          org-roam-ui-follow t
+          org-roam-ui-update-on-save t
+          org-roam-ui-open-on-start t))
+
+(use-package org-journal
+  :bind
+  (("C-c n n" . org-journal-new-entry)
+   ("C-c n s" . org-journal-new-date-entry)))
+
+(setq org-journal-dir (file-truename "~/Archive/Feronomicon/")
+      org-enable-org-journal-support t
+      org-journal-find-file #'find-file
+      org-journal-file-header "#+STARTUP: showeverything\n\n"
+      org-journal-file-format "%Y%m%d"
+      org-journal-date-prefix "#+TITLE: "
+      org-journal-date-format "%A  %Y-%m-%d"
+      org-journal-time-prefix "* "
+      org-journal-time-format "%H:%M"
+      org-journal-start-on-weekday 0)
+
+(require 'bibtex)
+
+(setq bibtex-autokey-year-length 4
+      bibtex-autokey-name-year-separator "-"
+      bibtex-autokey-year-title-separator "-"
+      bibtex-autokey-titleword-separator "-"
+      bibtex-autokey-titlewords 2
+      bibtex-autokey-titlewords-stretch 1
+      bibtex-autokey-titleword-length 5
+      bibtex-completion-format-citation-functions
+  '((org-mode      . bibtex-completion-format-citation-org-title-link-to-PDF)
+    (latex-mode    . bibtex-completion-format-citation-cite)
+    (markdown-mode . bibtex-completion-format-citation-pandoc-citeproc)
+    (default       . bibtex-completion-format-citation-default)))
+
+(define-key bibtex-mode-map (kbd "H-b") 'org-ref-bibtex-hydra/body)
+
+(use-package org-ref)
+
+(setq bibtex-completion-bibliography '("~/Archive/Apocrypha/reference-index.bib")
+      bibtex-completion-library-path '("~/Archive/Apocrypha/PDF/")
+      bibtex-completion-notes-path '("~/Archive/Grimoire")
+      bibtex-completion-pdf-extension '(".pdf" ".djvu", ".jpg")
+      bibtex-completion-browser-function 'browser-url-firefox
+      bibtex-completion-pdf-field "File"
+      bibtex-completion-notes-template-multiple-files "* ${author-or-editor}, ${title}, ${journal}, (${year}) :${=type=}: \n\nSee [[cite:&${=key=}]]\n"
+      bibtex-completion-additional-search-fields '(keywords)
+      bibtex-completion-display-formats
+        '((article       . "${=has-pdf=:1}${=has-note=:1} ${year:4} ${author:36} ${title:*} ${journal:40}")
+          (inbook        . "${=has-pdf=:1}${=has-note=:1} ${year:4} ${author:36} ${title:*} Chapter ${chapter:32}")
+          (incollection  . "${=has-pdf=:1}${=has-note=:1} ${year:4} ${author:36} ${title:*} ${booktitle:40}")
+          (inproceedings . "${=has-pdf=:1}${=has-note=:1} ${year:4} ${author:36} ${title:*} ${booktitle:40}")
+          (t             . "${=has-pdf=:1}${=has-note=:1} ${year:4} ${author:36} ${title:*}"))
+        bibtex-completion-pdf-open-function
+        (lambda (fpath)
+          (call-process "open" nil 0 nil fpath)))
+
+(require 'org-agenda)
+
+(setq org-agenda-files (append (directory-files-recursively "~/Archive/" "\\todo.org$"))
+      org-agenda-start-on-weekday 0
+      org-columns-default-format "%50ITEM(Task) %10CLOCKSUM %16TIMESTAMP_IA")
+
+(define-key org-agenda-mode-map (kbd "n") 'evil-next-line)
+(define-key org-agenda-mode-map (kbd "e") 'evil-previous-line)
+(define-key org-agenda-mode-map (kbd "n") 'org-agenda-next-line)
+(define-key org-agenda-mode-map (kbd "e") 'org-agenda-previous-line)
+(define-key org-agenda-mode-map (kbd "j") 'org-agenda-goto-date)
+(define-key org-agenda-mode-map (kbd "p") 'org-agenda-capture)
+(define-key org-agenda-mode-map (kbd "<SPC>") 'helm-occur)
+(define-key org-agenda-mode-map (kbd "s-A") 'org-agenda-exit)
+
+(use-package scad-mode)
+
+(use-package centered-cursor-mode)
+
+(use-package pdf-tools)
+
+(use-package dmenu)
+
+(use-package ag)
+
+(use-package rg)
+(global-set-key (kbd "C-s") #'rg-menu)
