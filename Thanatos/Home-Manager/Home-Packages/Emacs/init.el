0990fd42b0efbe456025ae6d2674b668631d6c11
xin <git@ironshark.org>
2023-11-20 16:40:35 -0500

    Rebuild:  307   2023-11-20 16:30:43    rien

--- Thanatos/Home-Manager/Home-Packages/Emacs/init.el
+++ Thanatos/Home-Manager/Home-Packages/Emacs/init.el
@@ -706,8 +706,8 @@ it can be passed in POS."
       org-export-with-todo-keywords nil
       org-reverse-note-order t
       org-odd-levels-only t
-      org-directory "~/Archive/Feronomicon/ORG/"
-      org-archive-location (format "~/Archive/Feronomicon/ORG/\%s-archive.org::datetree/" (Tn/current-year)))
+      org-directory "~/Archive/Feronomicon/ORG/")
+      ;org-archive-location (format "~/Archive/Feronomicon/ORG/\%s-archive.org::datetree/"
 
 (use-package org
 :hook
