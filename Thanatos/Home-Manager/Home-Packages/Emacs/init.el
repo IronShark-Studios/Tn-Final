24bdc0b84ac0a9313e2fd48b1e21b3737e74076d
xin <git@ironshark.org>
2023-11-22 17:08:37 -0500

    re added settings to config

--- Thanatos/Home-Manager/Home-Packages/Emacs/init.el
+++ Thanatos/Home-Manager/Home-Packages/Emacs/init.el
@@ -646,22 +646,23 @@ it can be passed in POS."
 (add-hook 'org-capture-mode-hook 'evil-insert-state)
 
 (setq org-todo-keywords
-      (quote ((sequence "TODO(t)" "NEXT(n)" "|" "INPROGRESS(i)" "DONE(d)")
+      (quote ((sequence "TODO(t)" "NEXT(n)" "|" "ACTIVE(a@/!)" "DONE(d@/!)")
               (sequence "WAITING(w@/!)" "HOLD(h@/!)" "|" "CANCELLED(c@/!)"))))
 
 (setq org-todo-keyword-faces
-      (quote (("TODO" :foreground "cyan" :weight bold)
+      (quote (("TODO" :foreground "dark blue" :weight bold)
               ("NEXT" :foreground "SeaGreen1" :weight bold)
-              ("INPROGRESS" :foreground "jade" :weight bold)
+              ("ACTIVE" :foreground "cyan" :weight bold)
               ("DONE" :foreground "dim gray" :weight bold)
-              ("WAITING" :foreground "deep pink" :weight bold)
-              ("HOLD" :foreground "blue violet" :weight bold)
-              ("CANCELLED" :foreground "dark red" :weight bold))))
+              ("WAITING" :foreground "blue violet" :weight bold)
+              ("HOLD" :foreground "dark red" :weight bold)
+              ("CANCELLED" :foreground "dim gray" :weight bold))))
 
 (setq org-ellipsis " ▾"
       org-hide-emphasis-markers t
       org-src-fontify-natively t
       org-fontify-quote-and-verse-blocks t
+      org-fontify-done-headline t
       org-src-tab-acts-natively t
       org-edit-src-content-indentation 2
       org-hide-block-startup nil
