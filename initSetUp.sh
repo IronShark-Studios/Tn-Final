87b21378d6546c4f399620d4f333f15d49cc1602
xin <git@ironshark.org>
2023-12-20 13:50:10 -0500

    added home directory and git repo setup to init script

--- initSetUp.sh
+++ initSetUp.sh
@@ -1,11 +1,43 @@
 #! /bin/sh
 
-echo "Creating System SSH-Key"
-mkdir ~/.ssh
-ssh-keygen -t rsa -b 4096 -N "" -f /home/xin/.ssh/id_rsa
+echo "This script is meant to be run after the initial set up of the local system"
+echo "Please press enter to continue"
+read -r ConfirmationInput
+echo
 
+echo "Configuring Home Directory"
+cd ~
+mkdir ~/.ssh
 mkdir ~/Archive
 mkdir ~/Projects
 mkdir ~/Media
+echo
 
+echo "Creating System SSH-Key"
+ssh-keygen -t rsa -b 4096 -N "" -f ~/.ssh/id_rsa
 cat ~/.ssh/id_rsa.pub
+echo
+
+echo "Please press enter after configuring git-source SSH."
+read -r ConfirmationInput
+echo
+
+echo "Configuring Personal Archives"
+cd ~/Archive
+git clone git@github.com:IronShark-Studios/Apocrypha.git
+git clone git@github.com:IronShark-Studios/Grimoire.git
+git clone git@github.com:IronShark-Studios/Feronomicon.git
+git clone git@github.com:IronShark-Studios/Tn-Final.git
+cd ~
+echo
+
+echo "Configuring Personal Projects"
+cd ~/Projects
+git clone git@github.com:IronShark-Studios/Iron-Shark.github.io.git
+mv Iron-Shark.github.io Personal-Blog
+cd ~
+echo
+
+echo
+echo
+echo "Home Directory Set Up Complete"
