ebe218980c2be4de6f0fdfdfae2bd59424d8d3d8
xin <git@ironshark.org>
2023-12-26 12:22:13 -0500

    misc

--- Thanatos/Home-Manager/Home-Packages/Zsh/alaises.nix
+++ Thanatos/Home-Manager/Home-Packages/Zsh/alaises.nix
@@ -4,14 +4,14 @@
     cd = "z";
     lx = "ls -la";
     ll = "ls -l";
-    rma = "move_to_tmp() { mv "$1" /tmp }";
+    rma = ''move_to_tmp() { mv "$1" /tmp }'';
     power-off = "sudo shutdown -h now";
     logout = "sudo kill -9 -1";
     restart = "sudo reboot";
     eo = "emacsclient -n";
     rebuild = "bash /etc/scripts/rebuild.sh";
     upgrade = "bash /etc/scripts/upgrade.sh";
-    test = "bash /etc/scripts/test.sh"
+    test = "bash /etc/scripts/test.sh";
     clean = "sudo nix-collect-garbage --delete-old && rebuild";
     cb = "xclip -selection clipboard";
   };
