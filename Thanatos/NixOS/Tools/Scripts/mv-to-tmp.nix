d8255751800395f531fb26ab78e4faa1a60aba61
xin <git@ironshark.org>
2024-01-01 21:07:17 -0500

    Rebuild:   46   2024-01-01 19:55:27

--- Thanatos/NixOS/Tools/Scripts/mv-to-tmp.nix
+++ Thanatos/NixOS/Tools/Scripts/mv-to-tmp.nix
@@ -5,15 +5,12 @@
     text = ''
       #!/bin/sh
 
-      # mv-to-tmp() {
-      #   mv "$1" /tmp
-      # }
-
       mv-to-tmp() {
         for file in "$@"; do
           mv "$file" /tmp || echo "Error: Failed to move '$file' to /tmp."
         done
       }
+
         mv-to-tmp "$@"
     '';
   };
