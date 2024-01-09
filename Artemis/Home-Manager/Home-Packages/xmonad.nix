787f0dcf22e0efc81d8b17f6b55b2dc4d74dfa98
xin <git@ironshark.org>
2024-01-09 13:55:06 -0500

    added volume management keybindings to xmonad

--- Artemis/Home-Manager/Home-Packages/xmonad.nix
+++ Artemis/Home-Manager/Home-Packages/xmonad.nix
@@ -8,6 +8,9 @@ import XMonad
 import XMonad.Util.Ungrab
 import XMonad.Util.EZConfig
 import XMonad.Util.SpawnOnce (spawnOnce)
+import Graphics.X11.ExtraTypes.XF86
+import XMonad.Layout.Hidden
+
 
 main = xmonad $ defaultConfig
     { modMask            = mod4Mask
@@ -20,9 +23,12 @@ main = xmonad $ defaultConfig
     `additionalKeys`
     [ ((mod4Mask, xK_x), spawn "dmenu_run")
     , ((mod4Mask, xK_c), kill)
-    , ((xK_<XF86AudioRaiseVolume>), raiseVolume 5)
-    , ((xK_<XF86AudioLowerVolume>), lowerVolume 5)
-    , ((xK_<XF86AudioMute>), toggleMute)
+    , ((mod4Mask .|. shiftMask, xK_period), spawn "wpctl set-volume @DEFAULT_AUDIO_SINK@ 5.0%+")
+    , ((mod4Mask .|. shiftMask, xK_comma), spawn "wpctl set-volume @DEFAULT_AUDIO_SINK@ 5.0%-")
+    , ((mod4Mask .|. shiftMask, xK_h), spawn "wpctl set-mute @DEFAULT_SINK@ toggle")
+    , ((0, xF86XK_AudioRaiseVolume), spawn "wpctl set-volume @DEFAULT_AUDIO_SINK@ 5.0%+")
+    , ((0, xF86XK_AudioLowerVolume), spawn "wpctl set-volume @DEFAULT_AUDIO_SINK@ 5.0%-")
+    , ((0, xF86XK_AudioMute), spawn "wpctl set-mute @DEFAULT_SINK@ toggle")
     , ((mod4Mask, xK_Escape), spawn "sudo slock")]
     '';
   };
